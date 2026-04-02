9bf81500df66c29f2bd32fc868ee87791bb5820a
Mostafa <mostafasad007@gmail.com>
2026-04-02 20:59:43 +0000

    hh

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if not st.session_state.authenticated:
     
     with col2:
         st.title("⚡ Welcome to Alex")
-        st.markdown("I am your AI Catalogue Assistant. Please sign in with the Company Master Key to proceed.")
+        st.markdown("I am your AI Catalogue Assistant. Please sign in with central password to proceed. Incase you do have it please reach @mostafa on slack ;)")
         st.write("---")
         
         with st.form("login_form"):
